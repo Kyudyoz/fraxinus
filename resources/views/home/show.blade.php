0015d4b7088242a21f802324bd47a230a5f1ccb8
Kyudyoz <kyudyozz84@gmail.com>
2023-05-29 20:30:47 +0700

    Menambah Modal Confirmation

--- resources/views/home/show.blade.php
+++ resources/views/home/show.blade.php
@@ -3,45 +3,94 @@
 
 
 <div class="itemz-desc showz">
-  <div class="itemz-desc-containerr">
-    <div class="box1">
-      <img src="{{ asset('storage/' . $product->image) }}" alt="img" style="height: 100%; width:100%" />
+    <div class="itemz-desc-containerr">
+        <div class="box1">
+            <img src="{{ asset('storage/' . $product->image) }}" alt="img" style="height: 100%; width:100%" />
+        </div>
+        <div class="box2">
+            <h1>{{ $product->name }}</h1>
+            <h2>Rp. {{ $product->price }}</h2>
+            <hr />
+            <p>Category: {{ $product->category }}</p>
+            <p>Seller: {{ $product->user->name }}</p>
+            <hr />
+            <p>{{ $product->description }}</p>
+            @if (auth()->user()->name != $product->user->name)
+            <div class="phone">
+                <a href="https://wa.me/{{ $product->user->phone }}">
+                    <h1><i class="fa-brands fa-whatsapp"></i>Hubungi Penjual</h1>
+                </a>
+            </div>
+            <form action="/wishlist/{{ $product->id }}" method="post">
+                @csrf
+                <div class="actions mb-2">
+                    <button type="submit"><i class="fa-solid fa-heart"></i>Wishlist</button>
+                </div>
+            </form>
+            @endif
+            @if (auth()->user()->name == $product->user->name)
+            <div class="actions">
+                <form action="/show/{{ $product->id }}" method="post" id="deleteForm">
+                    @method('delete')
+                    @csrf
+                    <!-- Tambahkan field hidden untuk melewatkan konfirmasi penghapusan -->
+                    <input type="hidden" name="confirmed" id="deleteConfirmed" value="0">
+                    <button type="button" class="btn btn-danger" onclick="confirmDelete(event)">Delete Product</button>
+                </form>
+            </div>
+            <a href="/show/{{ $product->id }}/edit" class="mt-3">Edit This Product</a>
+            @endif
+            <a href="/home" class="mt-3">All Products</a>
+        </div>
     </div>
-    <div class="box2">
-      <h1>{{ $product->name }}</h1>
-      <h2>Rp. {{ $product->price }}</h2>
-      <hr />
-      <p>Category: {{ $product->category }}</p>
-      <p>Seller: {{ $product->user->name }}</p>
-      <hr />
-      <p>{{ $product->description }}</p>
-      @if (auth()->user()->name != $product->user->name)
-      <div class="phone">
-        <a href="https://wa.me/{{ $product->user->phone }}">
-        <h1><i class="fa-brands fa-whatsapp"></i>Hubungi Penjual</h1>
-        </a>
-      </div>
-      <form action="/wishlist/{{ $product->id }}" method="post">
-      @csrf
-      <div class="actions mb-2" >
-        <button type="submit"><i class="fa-solid fa-heart"></i>Wishlist</button>
-      </div>
-      </form>
-      @endif     
-      @if (auth()->user()->name == $product->user->name)
-      <div class="actions">
-        <form action="/show/{{ $product->id }}" method="post">
-            @method('delete')
-            @csrf
-          <button type="submit" onclick="return confirm('Are You Sure?')">Delete Product</button>
-        </form>
-      </div>
-      <a href="/show/{{ $product->id }}/edit" class="mt-3">Edit This Product</a>
-      @endif
-
-      <a href="/home" class="mt-3">All Products</a>
-    </div>
-  </div>
 </div>
 
+<!-- Modal Konfirmasi Delete -->
+<div class="modal fade" id="confirmDeleteModal" tabindex="-1" aria-labelledby="confirmDeleteModalLabel"
+    aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="confirmDeleteModalLabel">Confirmation</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <p>Are you sure you want to delete this product?</p>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal"
+                    onclick="setDeleteConfirmed(0)">Cancel</button>
+                <button type="button" class="btn btn-danger" onclick="setDeleteConfirmed(1)"
+                    data-bs-dismiss="modal">Delete</button>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+<script>
+    // Fungsi untuk mengatur nilai konfirmasi penghapusan
+    function setDeleteConfirmed(value) {
+        document.getElementById('deleteConfirmed').value = value;
+    }
+
+    // Fungsi untuk menampilkan modal konfirmasi penghapusan
+    function confirmDelete(event) {
+        event.preventDefault();
+
+        // Tampilkan modal konfirmasi
+        $('#confirmDeleteModal').modal('show');
+
+        // Tambahkan event listener untuk menangani konfirmasi penghapusan
+        $('#confirmDeleteModal').on('hidden.bs.modal', function (e) {
+            // Dapatkan nilai konfirmasi
+            const confirmed = $('#deleteConfirmed').val();
+
+            // Lanjutkan dengan penghapusan jika dikonfirmasi
+            if (confirmed === '1') {
+                document.getElementById('deleteForm').submit();
+            }
+        });
+    }
+</script>
 @include('partials.footer1')
--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -10,10 +10,10 @@
                         @auth        
                         @if (auth()->user()->name == $post->user->name)
                         <a href="/posts/show/{{ $post->id }}/edit" class="btn btn-warning" style="vertical-align:bottom"><i class="fa-solid fa-pen-to-square" style="color: #000000;"></i> Edit</a>
-                        <form action="delete/{{ $post->id }}" method="post" class="d-inline">
+                        <form action="delete/{{ $post->id }}" method="post" class="d-inline" id="deleteForm">
                             @method('delete')
                             @csrf
-                            <button type="submit" class="btn btn-danger" onclick="return confirm('Are You Sure?')" style="vertical-align: bottom"><i class="fa-solid fa-trash" style="color: #000000;"></i> Hapus</button>
+                            <button type="button" class="btn btn-danger" onclick="confirmDelete(event)" style="vertical-align: bottom"><i class="fa-solid fa-trash" style="color: #000000;"></i> Hapus</button>
                           </form>
                         @endif 
                         @endauth 
@@ -39,4 +39,54 @@
                     <a href="/posts" class=" mt-2 mb-2 mx-3 btn btn-primary">Back to posts</a>
                 </div>
 </div>
+
+<!-- Modal Konfirmasi Delete -->
+<div class="modal fade" id="confirmDeleteModal" tabindex="-1" aria-labelledby="confirmDeleteModalLabel"
+    aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="confirmDeleteModalLabel">Confirmation</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <p>Are you sure you want to delete this post?</p>
+            </div>
+            <div class="modal-footer">
+                <input type="hidden" name="confirmed" id="deleteConfirmed" value="0">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal"
+                    onclick="setDeleteConfirmed(0)">Cancel</button>
+                <button type="button" class="btn btn-danger" onclick="setDeleteConfirmed(1)"
+                    data-bs-dismiss="modal">Delete</button>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+<script>
+    // Fungsi untuk mengatur nilai konfirmasi penghapusan
+    function setDeleteConfirmed(value) {
+        document.getElementById('deleteConfirmed').value = value;
+    }
+
+    // Fungsi untuk menampilkan modal konfirmasi penghapusan
+    function confirmDelete(event) {
+        event.preventDefault();
+
+        // Tampilkan modal konfirmasi
+        $('#confirmDeleteModal').modal('show');
+
+        // Tambahkan event listener untuk menangani konfirmasi penghapusan
+        $('#confirmDeleteModal').on('hidden.bs.modal', function (e) {
+            // Dapatkan nilai konfirmasi
+            const confirmed = $('#deleteConfirmed').val();
+
+            // Lanjutkan dengan penghapusan jika dikonfirmasi
+            if (confirmed === '1') {
+                document.getElementById('deleteForm').submit();
+            }
+        });
+    }
+</script>
 @include('partials.footer1')
